3b300961694e7eb11ccd0cc611c89abc17ac5d90
shevko_sasha <shevkosasha@gmail.com>
2021-12-23 13:03:33 +0300

    #4

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
-const {Schema, model} = require('mongoose');
-const course = require('./course');
+const {Schema, model} = require('mongoose')
 
 const userSchema = new Schema({
   email: {
@@ -28,23 +27,39 @@ const userSchema = new Schema({
   }
 })
 
-userSchema.methods.addToCart = function(course) {
-    const items = [...this.cart.items];
-    const idx = items.findIndex(c => {
-        return c.courseId.toString() === course._id.toString();
-    })
-    if (idx >= 0) {
-        items[idx].count = items[idx].count + 1;
-    } else {
-        items.push({
-            courseId: course._id,
-            count: 1,
-        })
-    }
 
-    // const newCart = {items: items}
-    this.cart = {items: items}
-    return this.save();
+userSchema.methods.addToCart = function(course) {
+  const items = [...this.cart.items]
+  const idx = items.findIndex(c => {
+    return c.courseId.toString() === course._id.toString()
+  })
+
+  if (idx >= 0) {
+    items[idx].count = items[idx].count + 1
+  } else {
+    items.push({
+      courseId: course._id,
+      count: 1
+    })
+  }
+
+  this.cart = {items}
+  return this.save()
+}
+
+
+userSchema.methods.removeFromCart = function(id) {
+  let items = [...this.cart.items]
+  const idx = items.findIndex(c => c.courseId.toString() === id.toString())
+
+  if (items[idx].count === 1) {
+    items = items.filter(c => c.courseId.toString() !== id.toString())
+  } else {
+    items[idx].count--
+  }
+
+  this.cart = {items}
+  return this.save()
 }
 
 module.exports = model('User', userSchema)
--- routes/card.js
+++ routes/card.js
@@ -4,7 +4,9 @@ const router = Router()
 
 function mapCartItems(cart) {
   return cart.items.map(c => ({
-    ...c.courseId._doc, count: c.count
+    ...c.courseId._doc, 
+    id: c.courseId.id,
+    count: c.count
   }))
 }
 
@@ -21,8 +23,17 @@ router.post('/add', async (req, res) => {
 })
 
 router.delete('/remove/:id', async (req, res) => {
-  const card = await Card.remove(req.params.id)
-  res.status(200).json(card)
+  // const card = await Card.remove(req.params.id)
+  // res.status(200).json(card)
+  await req.user.removeFromCart(req.params.id);
+  const user = await req.user.populate('cart.items.courseId');
+  const courses = mapCartItems(user.cart); 
+
+  const cart = {
+    courses, 
+    price: computePrice(courses),
+  }
+  res.status(200).json(cart)
 })
 
 router.get('/', async (req, res) => {
